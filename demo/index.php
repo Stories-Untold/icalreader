65e46b8c2edb6cb374f1ee38fa599e433a017f2c
David <294594564@qq.com>
2015-11-10 16:25:14 +0800

    #test webcal

--- demo/index.php
+++ demo/index.php
@@ -6,16 +6,18 @@ use Icalreader\Icalreader;
 
 function p($data)
 {
-    echo '<pre />';
-    print_r($data);
-    exit;
+	echo '<pre />';
+	print_r($data);
+	exit;
 }
 
 //$ical = new Icalreader("google_ical.ics");
 
 //$ical = new Icalreader("google_except_test.ics");
 
-$ical = new Icalreader("google_except_test2.ics");
+//$ical = new Icalreader("google_except_test2.ics");
+
+$ical = new Icalreader("webcal.ics");
 
 $events = $ical->events();
 
